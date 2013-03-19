a9e91ac822ef787c33b7806ffe1e4d05c0aeaf7c
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2013-03-19 22:44:36 +0100

    settings for collected static files added

--- sample_project/projects/django13/settings.py
+++ sample_project/projects/django13/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../../static-collected/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
