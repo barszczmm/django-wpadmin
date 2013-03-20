4a0ffbcda74f2bd33f42dec3cdfe1d8b7acd6e33
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2013-03-20 03:24:23 +0100

    LICENSE, MANIFEST.in, setup.py added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+from wpadmin import VERSION
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-wpadmin',
+    version=VERSION,
+    description='WordPress look and feel for Django administration panel.',
+    long_description = read('README.rst'),
+    author='Maciej Marczewski (barszcz)',
+    author_email='maciej@marczewski.net.pl',
+    url = 'https://github.com/barszczmm/django-wpadmin',
+    keywords = 'wordpress django admin',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe = False,
+)
--- wpadmin/__init__.py
+++ wpadmin/__init__.py
@@ -1,6 +1,6 @@
 """
-django-wpadmin is a collection of extensions/tools for the django-admin-tools
-app to make Django administration panel look and beave more like WordPress
+django-wpadmin is a collection of extensions/tools for the default Django
+administration panel which makes it look and behave more like WordPress
 administration panel.
 """
-VERSION = '0.1.0'
+VERSION = '0.1.1'
