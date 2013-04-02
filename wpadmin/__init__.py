9527a70c7fe765efbbdcb6cff05ec545410c9ed7
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2013-04-02 05:34:02 +0200

    version 0.2.0

--- wpadmin/__init__.py
+++ wpadmin/__init__.py
@@ -3,4 +3,4 @@ django-wpadmin is a collection of extensions/tools for the default Django
 administration panel which makes it look and behave more like WordPress
 administration panel.
 """
-VERSION = '0.1.2'
+VERSION = '0.2.0'
