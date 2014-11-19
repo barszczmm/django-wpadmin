4c1f0ecddf40bf27e3b65471269abe6bb6d14b71
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2014-11-19 23:46:56 +0100

    added some stacked and tabular inlines

--- test_project/apps/authors/admin.py
+++ test_project/apps/authors/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
+from books.models import Book
+from cds.models import Cd
+from dvds.models import Dvd
+
+
+class BookInlineAdmin(admin.TabularInline):
+    model = Book
+    extra = 0
+    readonly_fields = ('description', )
+
+
+class CdInlineAdmin(admin.StackedInline):
+    model = Cd
+
+
+class DvdInlineAdmin(admin.StackedInline):
+    model = Dvd
+
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    inlines = (BookInlineAdmin, CdInlineAdmin, DvdInlineAdmin)
