272910001bc398dcb36801197299f66732f371fb
Maciej 'barszcz' Marczewski <maciej@marczewski.net.pl>
2014-11-20 10:31:35 +0100

    version 1.6.2

--- wpadmin/__init__.py
+++ wpadmin/__init__.py
@@ -3,7 +3,7 @@ django_wpadmin is a collection of extensions/tools for the default Django
 administration panel which makes it look and behave more like WordPress
 administration panel.
 """
-VERSION = (1, 6, 1)
+VERSION = (1, 6, 2)
 
 try:
     from django import VERSION as DJANGO_VERSION
